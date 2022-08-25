1216a48d2e567c8f3c5d3b20efb68fb1e325158b
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-08-24 21:44:41 -0300

    Aritimetica

---
+++ primeiroProjeto/src/OperadoresAtirimeticos.java
@@ -0,0 +1,11 @@
+public class OperadoresAtirimeticos {
+    public static void main(String[] args) {
+        /*
+        *int n1 = 3;
+        *int n2 = 5;
+        *float m = (n1 + n2) / 2;
+        *System.out.format("A media foi de %.1f", m);
+        */
+
+    }
+}
