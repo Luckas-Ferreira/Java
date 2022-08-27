c49ea2875f4faddf0df6203db20edb798b2a4773
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-08-27 10:24:20 -0300

    aprendendo Do while

--- primeiroProjeto/src/EstruturaRepeticao.java
+++ primeiroProjeto/src/EstruturaRepeticao.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
 public class EstruturaRepeticao {
-    
+    public static void main(String[] args) {
+        /*
+        byte cc = 1;
+        while (cc < 5) {
+            System.out.printf("Cambalhota - %d \n", cc);
+            cc++;
+        }
+        */
+        Scanner teclado = new Scanner(System.in);
+        byte n, s = 0;
+        String resp;
+        do {
+            System.out.print("Digite um número: ");
+            n = teclado.nextByte();
+            s += n;
+            System.out.println("Quer continuar? [S/N]: ");
+            resp = teclado.next();
+        } while (resp.equals("S"));
+        System.out.printf("A soma de todos oos valores foi: %d", s);
+        teclado.close();
+    }
 }
