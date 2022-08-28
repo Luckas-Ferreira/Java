812efcf018a32c906b808553f70defc6e660b159
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-08-28 19:52:32 -0300

    aprendendo funções

--- primeiroProjeto/src/Vetores.java
+++ primeiroProjeto/src/Vetores.java
@@ -2,7 +2,7 @@ public class Vetores {
     public static void main(String[] args) {
         int n[] = {2, 5, 7, 9};
         System.out.println(" USANDO FOR");
-        for (int c = 0; c <= 3; c++) {
+        for (int c = 0; c < n.length; c++) {
             System.out.printf("Na posição %d está o numero %d \n", c, n[c]);
         }
         System.out.println("\n USANDO WHILE");
