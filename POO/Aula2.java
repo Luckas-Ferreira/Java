ff9c38a96af1f3e26b8eca799d3b8a9bd8f5e7b1
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-03 07:55:09 -0300

    .

---
+++ POO/Aula2.java
@@ -0,0 +1,8 @@
+public class Aula2 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.tampada = true;
+        c1.status();
+    }
+}
