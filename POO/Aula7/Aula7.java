4cf2da8eb0e0b13e5def728e069a90699eac9542
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-19 03:38:20 -0300

    atualização

--- POO/Aula7/Aula7.java
+++ POO/Aula7/Aula7.java
@@ -2,7 +2,12 @@ package Aula7;
 
 public class Aula7 {
     public static void main(String[] args) {
-        Lutador lutador1 = new Lutador();
-        lutador1.status();
+        Lutador lutador[] = new Lutador[6];
+        lutador[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+        lutador[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+        lutador[2] = new Lutador("Snapshodow", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
+        lutador[3] = new Lutador("Dead Code", "Autralia", 28, 1.93f, 81.6f, 13, 0, 2);
+        lutador[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
+        lutador[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
     }
 }
--- POO/Aula7/Lutador.java
+++ POO/Aula7/Lutador.java
@@ -1,97 +1,137 @@
 package Aula7;
 
-public class Lutador implements Ações{
+public class Lutador {
     private String name;
     private String nationality;
-    private int    age;
-    private float  height;
-    private float  weight;
+    private int age;
+    private float height;
+    private float weight;
     private String category;
-    private int    victory;
-    private int    defeat;
-    private int    draws;
+    private int victory;
+    private int defeat;
+    private int draws;
 
-    public String getName(){
+    public Lutador(String no, String na, int ag, float he, float we, int vi, int de, int dr){
+        this.name = no;
+        this.nationality = na;
+        this.age = ag;
+        this.height = he;
+        this.setWeight(we);
+        this.victory = vi;
+        this.defeat = de;
+        this.draws = dr;
+    }
+    public String getName() {
         return this.name;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    public String getNationality(){
+
+    public String getNationality() {
         return this.nationality;
     }
-    public void setNationality(String nationality){
+
+    public void setNationality(String nationality) {
         this.nationality = nationality;
     }
-    public int getAge(){
+
+    public int getAge() {
         return this.age;
     }
-    public void setAge(int age){
+
+    public void setAge(int age) {
         this.age = age;
     }
-    public float getHeight(){
+
+    public float getHeight() {
         return this.height;
     }
-    public void setHeight(float height){
+
+    public void setHeight(float height) {
         this.height = height;
     }
-    public float getWeight(){
+
+    public float getWeight() {
         return this.weight;
     }
-    public void setWeight(float weight){
+
+    public void setWeight(float weight) {
         this.weight = weight;
+        this.setCategory();
     }
-    public String getCategory(){
+
+    public String getCategory() {
         return this.category;
     }
-    public void setCategory(String category){
-        this.category = category;
+
+    private void setCategory() {
+        if (this.getWeight() < 52.2) {
+            this.category = "inválido";
+        }else if (this.getWeight() <= 70.3){
+            this.category = "Leve";
+        }else if (this.getWeight() <= 83.9){
+            this.category = "Médio";
+        }else if (this.getWeight() <= 120.2){
+            this.category = "Pesado";
+        }else {
+            this.category = "Inválido";
+        }
     }
-    public int getVictory(){
+
+    public int getVictory() {
         return this.victory;
     }
-    public void setVictory(int victory){
+
+    public void setVictory(int victory) {
         this.victory = victory;
     }
-    public int getDefeat(){
+
+    public int getDefeat() {
         return this.defeat;
     }
-    public void setDefeat(int defeat){
+
+    public void setDefeat(int defeat) {
         this.defeat = defeat;
     }
-    public int getDraws(){
+
+    public int getDraws() {
         return this.draws;
     }
-    public void setDraws(int draws){
+
+    public void setDraws(int draws) {
         this.draws = draws;
     }
-
-    @Override
+    
     public void presentation() {
         System.out.println("On this side we have " + getName());
-        System.out.println("Nationality: "         + getNationality());
-        System.out.println("Age: "                 + getAge());
-        System.out.println("Height: "              + getHeight());
-        System.out.println("Weight: "              + getWeight());
-        System.out.println("Victorys: "            + getVictory());
-        System.out.println("defeats: "             + getDefeat());
-        System.out.println("draws: "               + getDraws());
+        System.out.println("Nationality: " + getNationality());
+        System.out.println("Age: " + getAge());
+        System.out.println("Height: " + getHeight());
+        System.out.println("Weight: " + getWeight());
+        System.out.println("Victorys: " + getVictory());
+        System.out.println("defeats: " + getDefeat());
+        System.out.println("draws: " + getDraws());
     }
-    @Override
+
     public void status() {
-
+        System.out.println(this.getName() + "your weight is " + this.getCategory());
+        System.out.println("Wins: " + getVictory());
+        System.out.println("Loss: " + getDefeat());
+        System.out.println("Draws: " + getDraws());
     }
-    @Override
+
     public void winFight() {
- 
+        setVictory(getVictory() + 1);
     }
-    @Override
+
     public void lossFight() {
-
+        setDefeat(getDefeat() + 1);
     }
-    @Override
-    public void drawFight() {
 
+    public void drawFight() {
+        setDraws(getDraws() + 1);
     }
 
 }
