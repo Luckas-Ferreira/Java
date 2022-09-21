a37cdde8d4852a7dce2440429963daad13a415dc
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-21 18:44:47 -0300

    asdf

---
+++ POO/Aula9/src/Livro.java
@@ -0,0 +1,42 @@
+public class Livro {
+    private String  title;
+    private String  author;
+    private int     totalPages;
+    private boolean open;
+    private Pessoa  reader;
+
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getOuthor(){
+        return this.author;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public int getTotalPages(){
+        return this.totalPages;
+    }
+    public void setTotalPages(int totalPages){
+        this.totalPages = totalPages;
+    }
+    public boolean getOpen(){
+        return this.open;
+    }
+    public void setOpen(boolean open){
+        this.open = open;
+    }
+    public Pessoa getReader(){
+        return this.reader;
+    }
+    public void setReader(Pessoa reader){
+        this.reader = reader;
+    }
+    
+    public void details(){
+        
+    }
+}
