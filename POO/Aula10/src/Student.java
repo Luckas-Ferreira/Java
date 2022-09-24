06fea09c4312b69078f426aef61a4ac4ccb60b52
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-24 06:49:41 -0300

    asdf

--- POO/Aula10/src/Student.java
+++ POO/Aula10/src/Student.java
@@ -6,5 +6,17 @@ public class Student {
         
     }
 
+    public int getEnrollment(){
+        return this.enrollment;
+    }
+    public void setEnrollmente(int enrollment) {
+        this.enrollment = enrollment;
+    }
+    public String getCurse(){
+        return this.curse;
+    }
+    public void setCurse(String curse){
+        this.curse = curse;
+    }
     
 }
