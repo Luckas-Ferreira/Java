ca9c815c713e3576714758f61231ebe96f1730dd
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-24 06:54:40 -0300

    asdf

--- POO/Aula10/src/Pessoa.java
+++ POO/Aula10/src/Pessoa.java
@@ -1,3 +1,27 @@
 public class Pessoa {
-    
+    private String name;
+    private String sex;
+    private int    age;
+
+    public void birthday(){
+
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getSex(){
+        return this.sex;
+    }
+    public void setSex(String sex){
+        this.sex = sex;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
 }
