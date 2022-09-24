741192933377e6c25bf4b84705b0501c7dcdc066
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-24 07:03:50 -0300

    asdf

---
+++ POO/Aula10/src/Functionary.java
@@ -0,0 +1,20 @@
+public class Functionary {
+    private String sector;
+    private boolean working;
+
+    public void changeWork(){
+
+    }
+    public String getSector(){
+        return this.sector;
+    }
+    public void setSector(String sector){
+        this.sector = sector;
+    }
+    public boolean getWorking(){
+        return this.working;
+    }
+    public void setWorking(boolean working){
+        this.working = working;
+    }
+}
