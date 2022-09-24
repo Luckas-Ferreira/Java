442da155fc06bdcd469422e2b391437f07a93e1b
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-24 06:59:58 -0300

    teacher adder

---
+++ POO/Aula10/src/Teacher.java
@@ -0,0 +1,20 @@
+public class Teacher {
+    private String specialty;
+    private float  salary;
+
+    public void receiveRaise(){
+
+    }
+    public String getSpecialty(){
+        return this.specialty;
+    }
+    public void setSpecialty(String specialty){
+        this.specialty = specialty;
+    }
+    public float getSalary(){
+        return this.salary;
+    }
+    public void setSalary(float salary){
+        this.salary = salary;
+    }
+}
