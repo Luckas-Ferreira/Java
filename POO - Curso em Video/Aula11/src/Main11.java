e5c3d26da634d553f8a2ea7c738ebab42b064038
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-01 16:15:31 -0300

    ScholarShip adder

--- POO - Curso em Video/Aula11/src/Main11.java
+++ POO - Curso em Video/Aula11/src/Main11.java
@@ -2,7 +2,6 @@ package Aula11.src;
 
 public class Main11 {
     public static void main(String[] args) {
-        Person p1 = new Person();
         Teacher p2 = new Teacher();
         p2.setIncrease(500f);
         p2.setSalary(100f);
---
+++ POO - Curso em Video/Aula11/src/ScholarShip.java
@@ -0,0 +1,18 @@
+package Aula11.src;
+
+public class ScholarShip extends Student{
+    private float assistance;
+    
+    public  void renewBag(){
+
+    }
+    public void payMonthlyFee(){
+
+    }
+    public float getAssistance() {
+        return assistance;
+    }
+    public void setAssistance(float assistance) {
+        this.assistance = assistance;
+    }
+}
