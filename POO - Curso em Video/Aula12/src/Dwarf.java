722056eb8a5b411d5219a3c62a1830245edd94ac
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-01 19:28:14 -0300

    adsf

---
+++ POO - Curso em Video/Aula12/src/Dwarf.java
@@ -0,0 +1,3 @@
+public class Dwarf extends Ave{
+    
+}
---
+++ POO - Curso em Video/Aula12/src/Turtle.java
@@ -0,0 +1,8 @@
+public class Turtle extends Reptile{
+
+    @Override
+    public void move() {
+        System.out.println("Anda beeeem devagar");
+    }
+    
+}
