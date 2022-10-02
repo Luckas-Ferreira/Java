c493709e55df64234f673963a86748ed7cb538bc
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-02 19:14:23 -0300

    asdf

--- POO - Curso em Video/Aula13/src/Dog.java
+++ POO - Curso em Video/Aula13/src/Dog.java
@@ -7,12 +7,26 @@ public class Dog extends Wolf{
     }
 
     public void toReact(String phrase){
-
+        if (phrase.equals("tomar comida") || phrase.equals("Olá")){
+            System.out.println("Abanar e Latir");
+        }else {
+            System.out.println("Rosnar");
+        }
     }
     public void toReact(int hora, int minutes){
-
+        if (hora < 12){
+            System.out.println("Abanar");
+        }else if (hora >= 18){
+            System.out.println("Ignorar");
+        }else {
+            System.out.println("Abanar e Latir");
+        }
     }
     public void toReact(boolean person){
-        
+        if (person){
+            System.out.println("Abanar");
+        }else{
+            System.out.println("Rosnar e Latir");
+        }
     }
 }
