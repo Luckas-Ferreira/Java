472f9bff45b5646bc05e9246276cb66bb14b0051
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-02 19:34:56 -0300

    asdf

---
+++ POO - Curso em Video/Aula14/src/Person.java
@@ -0,0 +1,10 @@
+package Aula14.src;
+
+public abstract class Person {
+    protected String name;
+    protected int    age;
+    protected String sex;
+    protected String experience;
+
+    protected abstract void gainExp();
+}
