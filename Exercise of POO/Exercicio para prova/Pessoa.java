efabf24e67276a6aa71515da22b92fea8434790e
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-05 21:22:12 -0300

    asdf

---
+++ Exercise of POO/Exercicio para prova/Pessoa.java
@@ -0,0 +1,6 @@
+package Exercise of POO.Exercicio para prova;
+
+public abstract class Pessoa {
+    Private String nome;
+    Private int    idade;
+}
--- Exercise of POO/Exercicio para prova/Verification.java
+++
@@ -1,5 +0,0 @@
-package Exercise of POO.Exercicio para prova;
-
-public interface Verification {
-    
-}
