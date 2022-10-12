be45c4ab6e3eb41023ddcad95156fa23bb6a5c77
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-12 15:57:33 -0300

    Segunda questão comentada

--- Prova_POO/Questão2/Carro.java
+++ Prova_POO/Questão2/Carro.java
@@ -1,21 +1,23 @@
 package Questão2;
 
 public class Carro {
+    //Atributos usados.
     protected String modelo;
     protected int    capacidadeMaxima;
     protected int    quantidadePessoa;
     
+    //Construtor salva as informações nos atributos.
     public Carro(String modelo, int capacidadeMaxima, int quantidadePessoa){
-        if (quantidadePessoa <= capacidadeMaxima){
+        if (quantidadePessoa <= capacidadeMaxima){//Só será adicionado caso a quantidade de pessas seja menor ou igual a capacidade do carro.
             this.modelo = modelo;
             this.capacidadeMaxima = capacidadeMaxima;
             this.quantidadePessoa = quantidadePessoa;
-        }else{
+        }else{ //Caso a primeira condição não seja atendida, retorna uma frase.
             System.out.printf("Não é permitido mais que: %d pessoas no carro.", capacidadeMaxima);
         }
     }
 
-
+    //Getters and Setters dos atributos
     public String getModelo() {
         return modelo;
     }
@@ -40,7 +42,7 @@ public class Carro {
         this.quantidadePessoa = quantidadePessoa;
     }
 
-
+    //Mostra as informações na tela.
     @Override
     public String toString() {
         return "Carro: " + modelo + ", capacidade maxima: " + capacidadeMaxima + ", quantidade de pessoa: "
--- Prova_POO/Questão2/Estacionamento.java
+++ Prova_POO/Questão2/Estacionamento.java
@@ -1,24 +1,31 @@
 package Questão2;
 
+//Nessa questão, usamos ArrayList para adicionar os carros/quantidade de pessoas de forma dinâmica.
+//Assim sendo possivel listar todos os carros ou ver quantas pessoas existem. 
 import java.util.ArrayList;
+
 public class Estacionamento {
+    //Chama o objeto ArrayList passando a classe Carro.
     ArrayList<Carro> carros = new ArrayList<>();
     
+    //Adicionar um novo carro no ArrayList
     public void estacionar_Carro(Carro carro){
         carros.add(carro);
     }
     
+    //Faz a soma de todas as pessoas que estão dentro dos carros.
     public int verQuantidade_Pessoas_Estacionamento(){
-        int total_Pessoas = 0;
-        for (Carro carro : carros){
-            total_Pessoas += carro.getQuantidadePessoa();
+        int total_Pessoas = 0; //Armazenar a quantidade de pessoas.
+        for (Carro carro : carros){ //Chamada do Enhanced for-loop para percorrer e encontrar o carro na lista. 
+            total_Pessoas += carro.getQuantidadePessoa(); //Soma o valor anterior com o novo valor.
         }
-        System.out.printf("Tem %d pessoas no estacionamento \n", total_Pessoas);
+        System.out.printf("Tem %d pessoas no estacionamento \n", total_Pessoas); //Mostra todas as pessoas no estacionamento.
         return total_Pessoas;
     }
 
+    //Percorre toda a lista e mostra tudo que tem salva nela.
     public void listar_Carros(){
-        for (Carro carro : carros){
+        for (Carro carro : carros){ //Chamada Enhanced for-loop para percorrer toda a lista.
             System.out.println(carro);
         }
     }
